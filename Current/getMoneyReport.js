1132fed2ef11a1882c1e30461dc3ca764ccb4245
Bill-GD <geometrydashbill@gmail.com>
2023-05-18 09:48:36 +0700

    More suffixes & large number compatibility (if it works)
    - Added more suffixes (hopefully that it's enough)
    - Checks for money that is shown in this format: <num>e<exponent> (e.g. 1.234e+44)
     -> these are really big numbers, the game won't use letter suffixes but scientific notation instead

--- Current/getMoneyReport.js
+++ Current/getMoneyReport.js
@@ -1,6 +1,7 @@
 /** @param {NS} ns */
 export async function main(ns) {
-    // get the clicked nested element corresponding to the 'Stats' sidebar tab
+    // click the 'Stats' sidebar tab
+    // parent of the icon is clickable
     document.querySelector('svg[aria-label="Stats"]').parentElement.click();
 
     // get BitNode time
@@ -9,8 +10,7 @@ export async function main(ns) {
     for (const i in labels) {
         if (labels.item(i).textContent === 'Total')
             break;
-        else
-            bitnodeTime = labels.item(i).innerHTML;
+        else bitnodeTime = labels.item(i).innerHTML;
     }
 
     // get BitNode and level
@@ -38,11 +38,12 @@ export async function main(ns) {
                 .children.item(0); // inner table
     });
 
-    const suffices = ['k', 'm', 'b', 't'];
+    // need to get list of possible suffixes
+    const suffixes = ['k', 'm', 'b', 't', 'q', 'Q', 's', 'S', 'n'];
     let loss = 0, gain = 0;
-    let totalMoney = 0;
+    let lossStr = '', gainStr = '';
+    let totalMoney = 0, totalMoneyStr = '';
 
-    // let report = `BitNode: ${ns.getPlayer().bitNodeN}\n\n`;
     let report = `BitNode: ${bitnode}.${level}\n\n`;
 
     // get all items in the table
@@ -55,22 +56,37 @@ export async function main(ns) {
             .children.item(0)
             .children.item(0).innerHTML.substring(1);
 
-        const moneySuffix = money.substring(money.length - 1);
-        if (isNaN(parseInt(moneySuffix))) {
+        if (isNaN(parseInt(money.charAt(money.length - 1)))) {
+            const moneySuffix = money.substring(money.length - 1);
             money.substring(0, money.length - 1);
-            const power = suffices.findIndex(s => s === moneySuffix);
+            const power = suffixes.findIndex(s => s === moneySuffix);
             money = Math.round(parseFloat(money) * Math.pow(1e3, power + 1));
         }
-        else
-            money = parseFloat(money);
+        else if (!money.includes('e')) money = parseFloat(money);
 
-        money > 0 ? gain += money : loss += Math.abs(money);
-        totalMoney += money;
+        if (isNaN(parseFloat(money)) && money.includes('e')) {
+            money.includes('-') ? lossStr += money.substring(1) + '+' : gainStr += money + '+';
+            totalMoneyStr += money + '+';
+        }
+        else {
+            money > 0 ? gain += money : loss += Math.abs(money);
+            totalMoney += money;
+        }
 
         report += `${title} $${ns.formatNumber(money)}\n`;
     }
 
-    report += `\nLoss: ${loss} (- ${ns.formatNumber(loss)})\nGain: ${gain} (+ ${ns.formatNumber(gain)})\nTotal: ${totalMoney} (${ns.formatNumber(totalMoney)})\n\nTime: ${bitnodeTime}`;
+    gainStr.length > 0 ? gainStr += '\n' : 0;
+    lossStr.length > 0 ? lossStr += '\n' : 0;
+    totalMoneyStr.length > 0 ? totalMoneyStr += '\n' : 0;
+
+    report += `\nLoss: ${loss} (-${ns.formatNumber(loss)})\n` +
+        `${lossStr}` +
+        `Gain: ${gain} (+${ns.formatNumber(gain)})\n` +
+        `${gainStr}` +
+        `Total: ${totalMoney} (${ns.formatNumber(totalMoney)})\n` +
+        `${totalMoneyStr}\n` +
+        `Time: ${bitnodeTime}`;
 
     ns.write('BitNode_Money_Report.txt', report, 'w');
 
