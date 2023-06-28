06b6393a4cd6316d211948611e126573201650fe
Bill-GD <geometrydashbill@gmail.com>
2023-06-28 15:02:53 +0700

    Replaced getServer by getServerMaxRam -> less RAM

--- Current/homeUpgrade.js
+++ Current/homeUpgrade.js
@@ -1,7 +1,7 @@
 /** @param {NS} ns */
 export async function main(ns) {
   if (!(await ns.prompt(
-    `Upgrade RAM: ${ns.formatRam(ns.getServer('home').maxRam, 2)} -> ${ns.formatRam(ns.getServer('home').maxRam * 2, 2)}\n` +
+    `Upgrade RAM: ${ns.formatRam(ns.getServerMaxRam('home'), 2)} -> ${ns.formatRam(ns.getServerMaxRam('home') * 2, 2)}\n` +
     `Cost: $${ns.formatNumber(ns.singularity.getUpgradeHomeRamCost(), 3)}`
   ))) ns.exit();
 
