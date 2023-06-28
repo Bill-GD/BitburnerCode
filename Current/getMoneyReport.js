7e5ef05ae7ba5195fa9a56fa28fc33a307dadc5d
Bill-GD <geometrydashbill@gmail.com>
2023-06-28 07:20:43 +0700

    Removed DOM method for reporting income

--- Current/getMoneyReport.js
+++ Current/getMoneyReport.js
@@ -1,84 +1,34 @@
-/** Version 1.3.1
- * Fixed inconsistent display
+/** Version 1.4
+ * Removed HTML (DOM/document) method completely
+ * Display using alert box and save to file for later viewing
  */
 /** @param {NS} ns */
 export async function main(ns) {
-  const doc = eval('document');
-  // click the 'Stats' sidebar tab
-  // parent of the icon is clickable
-  try {
-    doc.querySelector('svg[aria-label="Stats"]').parentElement.click();
+  const saveData = JSON.parse(JSON.parse(atob(eval('window').appSaveFns.getSaveData().save)).data.PlayerSave).data;
 
-    // get BitNode time
-    const labels = doc.getElementsByClassName('MuiTypography-root');
-    let bitnodeTime = '';
-    for (const i in labels) {
-      if (labels.item(i).textContent === 'Total')
-        break;
-      else bitnodeTime = labels.item(i).innerHTML;
-    }
+  const bitnodeTime = ns.tFormat(saveData.playtimeSinceLastBitnode);
 
-    // get BitNode and level
-    let bitnode = '', level = '';
-    for (const i in labels) {
-      const text = labels.item(i).textContent;
-      if (text.includes('BitNode') && text.includes('(Level')) {
-        bitnode = text.split(':')[0].split(' ')[1];
-        level = text.split('(')[1].split(' ')[1].split(')')[0];
-        break;
-      }
-    }
-    let report = [`BitNode: ${bitnode}.${level}\n`];
+  const bitnode = saveData.bitNodeN;
+  let level = saveData.sourceFiles.data.find(e => e[0] === bitnode)[1];
+  if (bitnode === 12) level += 1;
+  else if (level < 3) level += 1;
+  let report = [`BitNode: ${bitnode}.${level}\n`];
 
-    let loss = 0, gain = 0;
-    let totalMoney = 0;
+  const moneySinceBN = saveData.moneySourceB.data;
+  let loss = 0, gain = 0;
 
-    Object.entries(ns.getMoneySources().sinceStart).forEach(([k, v]) => {
-      if (k === 'total') return;
-      totalMoney += v;
-      if (v !== 0) report.push(`${k.charAt(0).toUpperCase() + k.substring(1)}: $${ns.formatNumber(v, 3)}`);
-      v > 0 ? gain += v : loss += Math.abs(v);
-    });
+  Object.entries(moneySinceBN).forEach(([k, v]) => {
+    if (k === 'total') return;
+    if (v !== 0) report.push(`${k.charAt(0).toUpperCase() + k.substring(1)}: $${ns.formatNumber(v, 3)}`);
+    v > 0 ? gain += v : loss += Math.abs(v);
+  });
 
-    report.push(`\nLoss: ${loss} ($${ns.formatNumber(loss, 3)})\n` +
-      `Gain: ${gain} ($${ns.formatNumber(gain, 3)})\n` +
-      `Total: ${totalMoney} ($${ns.formatNumber(totalMoney, 3)})\n\n` +
-      `Time: ${bitnodeTime}`);
+  report.push(`\nLoss: ${loss} ($${ns.formatNumber(loss, 3)})\n` +
+    `Gain: ${gain} ($${ns.formatNumber(gain, 3)})\n` +
+    `Total: ${moneySinceBN.total} ($${ns.formatNumber(moneySinceBN.total, 3)})\n\n` +
+    `Time: ${bitnodeTime}`);
 
-    ns.write('BitNode_Money_Report.txt', report.join('\n'), 'w');
+  ns.alert(report.join('\n') + `\n\nSaved to 'BitNode_Money_Report.txt'`);
 
-    // return to terminal
-    doc.querySelector('svg[aria-label="Terminal"]').parentElement.click();
-
-    // open report
-    const terminalInput = doc.getElementById('terminal-input');
-    terminalInput.value = 'cat BitNode_Money_Report.txt';
-    const handler = Object.keys(terminalInput)[1];
-    terminalInput[handler].onChange({ target: terminalInput });
-    terminalInput[handler].onKeyDown({ key: 'Enter', preventDefault: () => null });
-  } catch (error) { // can't use sidetabs and/or terminal (when focus)
-    const saveData = JSON.parse(JSON.parse(atob(eval('window').appSaveFns.getSaveData().save)).data.PlayerSave).data;
-
-    const bitnodeTime = ns.tFormat(saveData.playtimeSinceLastBitnode);
-
-    const bitnode = saveData.bitNodeN;
-    const level = saveData.sourceFiles.data.find(e => e[0] === bitnode)[1];
-    let report = [`BitNode: ${bitnode}.${level}\n`];
-
-    const moneySinceBN = saveData.moneySourceB.data;
-    let loss = 0, gain = 0;
-
-    Object.entries(moneySinceBN).forEach(([k, v]) => {
-      if (k === 'total') return;
-      if (v !== 0) report.push(`${k.charAt(0).toUpperCase() + k.substring(1)}: $${ns.formatNumber(v, 3)}`);
-      v > 0 ? gain += v : loss += Math.abs(v);
-    });
-
-    report.push(`\nLoss: ${loss} ($${ns.formatNumber(loss, 3)})\n` +
-      `Gain: ${gain} ($${ns.formatNumber(gain, 3)})\n` +
-      `Total: ${moneySinceBN.total} ($${ns.formatNumber(moneySinceBN.total, 3)})\n\n` +
-      `Time: ${bitnodeTime}`);
-    
-    ns.alert(report.join('\n'));
-  }
+  ns.write('BitNode_Money_Report.txt', report.join('\n'), 'w');
 }
