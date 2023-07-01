3b2c4ad63a92e7a2e651b102c267ce251b4043ff
Bill-GD <geometrydashbill@gmail.com>
2023-07-01 15:11:20 +0700

    Changed method to acquire BitNode and level

--- Current/stanek_v2.js
+++ Current/stanek_v2.js
@@ -1,67 +1,67 @@
-/** Version 2.0.3
- * Added placement for size [7,6]
+/** Version 2.0.4
+ * Now uses the same method as HUD to get BitNode and level
  */
 /** @param {NS} ns */
 export async function main(ns) {
+  const doc = eval('document');
+  let level = '';
   try {
-    const doc = eval('document');
     doc.querySelector('svg[aria-label="Stats"]').parentElement.click();
+
     const labels = doc.getElementsByClassName('MuiTypography-root');
-    let bitnode = '', level = '';
+    // get level
     for (const i in labels) {
       const text = labels.item(i).textContent;
       if (text.includes('BitNode') && text.includes('(Level')) {
-        bitnode = text.split(':')[0].split(' ')[1];
-        level = text.split('(')[1].split(' ')[1].charAt(0);
+        level = text.split('(')[1].split(' ')[1].split(')')[0];
         break;
       }
     }
     doc.querySelector('svg[aria-label="Terminal"]').parentElement.click();
-    const size = getSize(parseInt(bitnode), parseInt(level));
+  } catch {
+    level = ns.read('BN_Level.txt');
+  }
 
-    const Fragments = {
-      Strength: 10,
-      Defense: 12,
-      Dexterity: 14,
-      Agility: 16,
-      Blade: 30,
-      Crime: 28,
-      HacknetProd: 20,
-    };
+  const currentBN = JSON.parse(JSON.parse(atob(eval('window').appSaveFns.getSaveData().save)).data.PlayerSave).data.bitNodeN;
+  const size = getSize(parseInt(currentBN), parseInt(level));
 
-    if (ns.stanek.acceptGift())
-      ns.alert(`Accepted Stanek's gift`);
-    else {
-      ns.alert(`Failed to accept gift`);
-      ns.exit();
-    }
+  const Fragments = {
+    Strength: 10,
+    Defense: 12,
+    Dexterity: 14,
+    Agility: 16,
+    Blade: 30,
+    Crime: 28,
+    HacknetProd: 20,
+  };
 
-    // rotation parameter uses 'E' rotation
-    if (compareSize(size, [7, 7])) {
-      ns.stanek.placeFragment(0, 0, 3, Fragments.Strength);
-      ns.stanek.placeFragment(2, 2, 0, Fragments.Agility);
-      ns.stanek.placeFragment(1, 0, 3, Fragments.Defense);
-      ns.stanek.placeFragment(0, 5, 0, Fragments.Dexterity);
-      ns.stanek.placeFragment(4, 5, 0, Fragments.Blade);
-      ns.stanek.placeFragment(5, 2, 3, Fragments.Crime);
-      ns.stanek.placeFragment(0, 3, 0, 106);
-      ns.stanek.placeFragment(3, 3, 0, 105);
-      ns.stanek.placeFragment(3, 0, 2, 101);
-    }
-    if (compareSize(size, [7, 6])) {
-      ns.stanek.placeFragment(2, 2, 0, 101);
-      ns.stanek.placeFragment(0, 4, 0, Fragments.Blade);
-      ns.stanek.placeFragment(3, 3, 1, Fragments.Crime);
-      ns.stanek.placeFragment(4, 3, 1, 105);
-      ns.stanek.placeFragment(6, 1, 1, Fragments.HacknetProd);
-      ns.stanek.placeFragment(4, 0, 0, Fragments.Agility);
-      ns.stanek.placeFragment(2, 0, 0, Fragments.Strength);
-      ns.stanek.placeFragment(0, 0, 3, Fragments.Dexterity);
-      ns.stanek.placeFragment(0, 1, 1, Fragments.Defense);
-    }
-    
-  } catch (error) {
-    ns.alert(`Can't access BitNode detail\nMaybe you're focusing on work/crime\n` + error.message);
+  if (!ns.stanek.acceptGift()) {
+    ns.alert(`Failed to accept gift`);
+    ns.exit();
+  }
+
+  // rotation parameter uses 'E' rotation
+  if (compareSize(size, [7, 7])) {
+    ns.stanek.placeFragment(0, 0, 3, Fragments.Strength);
+    ns.stanek.placeFragment(2, 2, 0, Fragments.Agility);
+    ns.stanek.placeFragment(1, 0, 3, Fragments.Defense);
+    ns.stanek.placeFragment(0, 5, 0, Fragments.Dexterity);
+    ns.stanek.placeFragment(4, 5, 0, Fragments.Blade);
+    ns.stanek.placeFragment(5, 2, 3, Fragments.Crime);
+    ns.stanek.placeFragment(0, 3, 0, 106);
+    ns.stanek.placeFragment(3, 3, 0, 105);
+    ns.stanek.placeFragment(3, 0, 2, 101);
+  }
+  if (compareSize(size, [7, 6])) {
+    ns.stanek.placeFragment(2, 2, 0, 101);
+    ns.stanek.placeFragment(0, 4, 0, Fragments.Blade);
+    ns.stanek.placeFragment(3, 3, 1, Fragments.Crime);
+    ns.stanek.placeFragment(4, 3, 1, 105);
+    ns.stanek.placeFragment(6, 1, 1, Fragments.HacknetProd);
+    ns.stanek.placeFragment(4, 0, 0, Fragments.Agility);
+    ns.stanek.placeFragment(2, 0, 0, Fragments.Strength);
+    ns.stanek.placeFragment(0, 0, 3, Fragments.Dexterity);
+    ns.stanek.placeFragment(0, 1, 1, Fragments.Defense);
   }
 
   /** @returns {[number, number]} Returns ```[width, height]```. */
