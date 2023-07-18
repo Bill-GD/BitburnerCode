cb09ad113404a38e8f890e0f8c353e5ba3081902
Bill-GD <geometrydashbill@gmail.com>
2023-07-18 19:41:38 +0700

    Changed format
    Easier to paste into sheets/excel

--- Current/intLog.js
+++ Current/intLog.js
@@ -5,29 +5,37 @@ export async function main(ns) {
     delayHour = 4; // time between each log entry (in hours)
   while (1) {
     const file = ns.read(fileName);
-    let [time, int] = ['', ''],
-      [level, xp, rate] = ['', '', ''];
+    let [time, level, xp, rate] = ['', '', '', '', '', ''];
 
     let [currentLevel, currentXp] = [ns.getPlayer().skills.intelligence, ns.getPlayer().exp.intelligence];
 
-    if (file.match(/[\[\]]/)) {
+    if (file.match(/[M\/\:]/)) {
       const latest = file.split('\n').filter(line => line !== '').reverse()[0];
-      // const sample = '[7/13/2023, 6:55:42 PM] lv, xp (readable xp)' + ', rate';
+      // const sample = '7/13/2023, 6:55:42 PM, lv, xp (readable xp)' + ', rate';
 
-      [_, time, int] = latest.split(/[\[\]]/);
-      [level, xp] = int.trim().split(/[, ]/).filter(e => e !== '');
+      [time, level, xp] = latest.split(/,/).map(e => e.trim());
+      xp = xp.split(' ')[0];
 
       const oldTime = (new Date(time)).getTime();
-      const timeDiff = (Date.now() - oldTime) / 60e3; // max precision: [milisecond] - [second] -> minute
+      const timeDiff = (Date.now() - oldTime);
       const [levelDiff, xpDiff] = [currentLevel - parseInt(level), currentXp - parseFloat(xp)];
-      const [lvRate, xpRate] = [levelDiff / timeDiff, xpDiff / timeDiff]; // per hour
-      rate = `, ${ns.formatNumber(lvRate, 3)} lv/m, ${ns.formatNumber(xpRate, 3)} xp/m`;
+      const [lvRate, xpRate] = [levelDiff / timeDiff * 3600e3, xpDiff / timeDiff * 1e3]; // per minute, second
+      rate = `, ${ns.formatNumber(lvRate, 3)} lv/h, ${ns.formatNumber(xpRate, 3)} xp/s`;
     }
 
     [currentLevel, currentXp] = [ns.getPlayer().skills.intelligence, ns.getPlayer().exp.intelligence];
-    const string = `[${(new Date()).toLocaleString()}] ${currentLevel}, ${currentXp} (${ns.formatNumber(currentXp, 3)})${rate}\n`;
+    const string = `${getTimeString(new Date())}, ${currentLevel}, ${currentXp} (${ns.formatNumber(currentXp, 3)})${rate}\n`;
     ns.write(fileName, string, 'a');
-    ns.print(`Logged at: ${(new Date()).toLocaleString()}`);
+    ns.print(`Logged at: ${getTimeString(new Date())}`);
     await ns.sleep(delayHour * 3600e3);
   }
+}
+
+function getTimeString(time) {
+  const date = new Date(time);
+  let [hour, minute, second] = [date.getHours(), date.getMinutes(), date.getSeconds()];
+  hour = hour >= 10 ? hour : '0' + hour;
+  minute = minute >= 10 ? minute : '0' + minute;
+  second = second >= 10 ? second : '0' + second;
+  return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()} ${hour}:${minute}:${second}`;
 }
