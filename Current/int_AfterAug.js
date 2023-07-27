7e0834bcd42ef690606f3f56f9218d98af817c74
Bill-GD <geometrydashbill@gmail.com>
2023-07-27 10:37:24 +0700

    Avoid duplicate after soft-reset (int)
    Also wait for gang money

--- Current/int_AfterAug.js
+++ Current/int_AfterAug.js
@@ -1,15 +1,17 @@
 /** @param {NS} ns */
 export async function main(ns) {
-  ns.run('hudExtra_v2.js');
-  ns.run('gang_v3.js');
-  ns.run('blade_v4.js');
-  ns.run('intTravel.js');
-  ns.run('intLog.js');
-  ns.run('buyNFG.js');
+  ns.run('hudExtra_v2.js', { preventDuplicates: true });
+  ns.run('gang_v3.js', { preventDuplicates: true });
+  ns.run('blade_v4.js', { preventDuplicates: true });
+  ns.run('intTravel.js', { preventDuplicates: true });
+  ns.run('intLog.js', { preventDuplicates: true });
+  ns.run('buyNFG.js', { preventDuplicates: true });
   ns.run('sleevePresets.js', { preventDuplicates: true }, '--script', '--preset', 'Infiltrate');
-  ns.run('int_SleeveC-NFG.js');
+  ns.run('int_SleeveC-NFG.js', { preventDuplicates: true });
+  await ns.sleep(6000);
   ns.run('extraServer_v2.js', { preventDuplicates: true }, '--script');
-  ns.run('stanek_v2.js', 'home', { preventDuplicates: true });
-  ns.run('manageChargeStanek.js', 'home', { preventDuplicates: true });
-  ns.run('spreadStanekCharger.js', 'home', { preventDuplicates: true });
+  await ns.sleep(50);
+  ns.run('stanek_v2.js', { preventDuplicates: true });
+  ns.run('manageChargeStanek.js', { preventDuplicates: true });
+  ns.run('spreadStanekCharger.js', { preventDuplicates: true });
 }
