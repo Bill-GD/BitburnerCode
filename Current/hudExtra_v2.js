5d46265c9095fc115fc393d46d20c06754ac117c
Bill-GD <geometrydashbill@gmail.com>
2023-08-07 18:27:06 +0700

    HUD update
    - Grouped some options together
    - Added BN Multipliers

--- Current/hudExtra_v2.js
+++ Current/hudExtra_v2.js
@@ -1,5 +1,6 @@
-/** Version 2.2.13
- * Uses the script option for 'sleevePresets.js' -> delay tail
+/** Version 2.3
+ * Grouped some options together
+ * Added BN Multipliers
  */
 /** @param {NS} ns */
 export async function main(ns) {
@@ -81,162 +82,70 @@ export async function main(ns) {
   };
 
   const optionStates = {
-    player: false,
+    info: false,
     crime: false,
     gang: false,
     stock: false,
-    hacking: false,//
-    manageServers: false,
-    graft: false,
-    graftMultiple: false,
+    hacking: false,
+    grafting: false,
+    bnMult: false,
     blade: false,
     stanek: false,
     sleeve: false,
     corp: false,
     runScript: false,
     traveling: false,
-    showRam: false,
+    manageServers: false,
     buyRam: false,
-    reportMoney: false,
     clearTerminal: false,
-    hudRestart: false,
-    hudExit: false,
+    reportMoney: false,
+    hud: false,
   };
 
-  // player
-  createElement(
-    'button',
-    { innerHTML: 'Player', style: { color: theme.hp } },
-    'click', () => optionStates.player = !optionStates.player,
-    hooks.hookHP
-  );
-
   // crime
-  createElement(
-    'button',
-    { innerHTML: 'Crime', style: { color: theme.hp } },
-    'click', () => optionStates.crime = true,
-    hooks.hookHP
-  );
+  createElement('button', { innerHTML: 'Crime', style: { color: theme.hp } }, 'click', () => optionStates.crime = true, hooks.hookHP);
 
   // gang
-  createElement(
-    'button',
-    { innerHTML: 'Gang', style: { color: theme.money } },
-    'click', () => optionStates.gang = true,
-    hooks.hookMoney
-  );
+  createElement('button', { innerHTML: 'Gang', style: { color: theme.hp } }, 'click', () => optionStates.gang = true, hooks.hookHP);
 
   // stock
-  createElement(
-    'button',
-    { innerHTML: 'Stock', style: { color: theme.money } },
-    'click', () => optionStates.stock = true,
-    hooks.hookMoney
-  );
+  createElement('button', { innerHTML: 'Stock', style: { color: theme.money } }, 'click', () => optionStates.stock = true, hooks.hookMoney);
 
   // hack
-  createElement(
-    'button',
-    { innerHTML: 'Hack', style: { color: theme['hack'] } },
-    'click', () => optionStates.hacking = true,
-    hooks.hookHack
-  );
-
-  // manage servers
-  createElement(
-    'button',
-    { innerHTML: 'Servers', style: { color: theme['hack'] } },
-    'click', () => optionStates.manageServers = true,
-    hooks.hookHack
-  );
+  createElement('button', { innerHTML: 'Hack', style: { color: theme['hack'] } }, 'click', () => optionStates.hacking = true, hooks.hookHack);
 
   // grafting
-  createElement(
-    'button',
-    { innerHTML: 'Graft', style: { color: theme.combat } },
-    'click', () => optionStates.graft = true,
-    hooks.hookStr
-  );
+  createElement('button', { innerHTML: 'Graft', style: { color: theme.combat } }, 'click', () => optionStates.grafting = true, hooks.hookStr);
 
-  // graft multiple
-  createElement(
-    'button',
-    { innerHTML: 'Multi', style: { color: theme.combat } },
-    'click', () => optionStates.graftMultiple = true,
-    hooks.hookStr
-  );
+  // BN mults
+  createElement('button', { innerHTML: 'BN Mults', style: { color: theme.combat } }, 'click', () => optionStates.bnMult = true, hooks.hookStr);
 
   // blade
-  createElement(
-    'button',
-    { innerHTML: 'Blade', style: { color: theme.combat } },
-    'click', () => optionStates.blade = true,
-    hooks.hookDef
-  );
+  createElement('button', { innerHTML: 'Blade', style: { color: theme.combat } }, 'click', () => optionStates.blade = true, hooks.hookDef);
 
-  createElement(
-    'button',
-    { innerHTML: 'Stanek', style: { color: theme.combat } },
-    'click', () => optionStates.stanek = true,
-    hooks.hookDef
-  );
+  // stanek
+  createElement('button', { innerHTML: 'Stanek', style: { color: theme.combat } }, 'click', () => optionStates.stanek = true, hooks.hookDef);
 
   // sleeve
-  createElement(
-    'button',
-    { innerHTML: 'Sleeve', style: { color: theme.combat } },
-    'click', () => optionStates.sleeve = true,
-    hooks.hookDex
-  );
+  createElement('button', { innerHTML: 'Sleeve', style: { color: theme.combat } }, 'click', () => optionStates.sleeve = true, hooks.hookDex);
 
   // corp
-  createElement(
-    'button',
-    { innerHTML: 'Corp', style: { color: theme.combat } },
-    'click', () => optionStates.corp = true,
-    hooks.hookDex
-  );
+  createElement('button', { innerHTML: 'Corp', style: { color: theme.combat } }, 'click', () => optionStates.corp = true, hooks.hookDex);
 
   // run script
-  createElement(
-    'button',
-    { innerHTML: 'Script', style: { color: theme.combat } },
-    'click', () => optionStates.runScript = true,
-    hooks.hookAgi
-  );
+  createElement('button', { innerHTML: 'Script', style: { color: theme.combat } }, 'click', () => optionStates.runScript = true, hooks.hookAgi);
 
   // traveling
-  createElement(
-    'button',
-    { innerHTML: 'Travel', style: { color: theme.combat } },
-    'click', () => optionStates.traveling = true,
-    hooks.hookAgi
-  );
+  createElement('button', { innerHTML: 'Travel', style: { color: theme.combat } }, 'click', () => optionStates.traveling = true, hooks.hookAgi);
 
-  // ram usage
-  createElement(
-    'button',
-    { innerHTML: 'Usage', style: { color: theme.cha } },
-    'click', () => optionStates.showRam = !optionStates.showRam,
-    hooks.hookCha
-  );
+  // manage servers
+  createElement('button', { innerHTML: 'Servers', style: { color: theme.cha } }, 'click', () => optionStates.manageServers = true, hooks.hookCha);
 
   // buy ram
-  createElement(
-    'button',
-    { innerHTML: 'RAM', style: { color: theme.cha } },
-    'click', () => optionStates.buyRam = true,
-    hooks.hookCha
-  );
+  createElement('button', { innerHTML: 'RAM', style: { color: theme.cha } }, 'click', () => optionStates.buyRam = true, hooks.hookCha);
 
-  // money report
-  createElement(
-    'button',
-    { innerHTML: 'Report', style: { color: theme.int } },
-    'click', () => optionStates.reportMoney = true,
-    hooks.hookInt
-  );
+  // info
+  createElement('button', { innerHTML: 'Info', style: { color: theme.int } }, 'click', () => optionStates.info = !optionStates.info, hooks.hookInt);
 
   // cls;ls
   createElement(
@@ -252,19 +161,11 @@ export async function main(ns) {
     hooks.hookInt
   );
 
+  // money report
+  createElement('button', { innerHTML: 'Report', style: { color: theme.warning } }, 'click', () => optionStates.reportMoney = true, hooks.hook2);
+
   // restart & exit
-  createElement(
-    'button',
-    { innerHTML: 'Restart', style: { color: theme.warning } },
-    'click', () => optionStates.hudRestart = true,
-    hooks.hook2
-  );
-  createElement(
-    'button',
-    { innerHTML: 'Remove', style: { color: theme.warning } },
-    'click', () => optionStates.hudExit = true,
-    hooks.hook2
-  );
+  createElement('button', { innerHTML: 'HUD', style: { color: theme.warning } }, 'click', () => optionStates.hud = true, hooks.hook2);
 
   buttons.forEach(bt => Object.assign(bt.style, { width: `${maxTextWidth * 10}px` }));
 
@@ -272,116 +173,119 @@ export async function main(ns) {
   const currentBNLevel = ns.read('BN_Level.txt');
 
   while (await ns.sleep(10)) {
-    try {
-      let key = ['BitNode'];
-      let value = [`${currentBN}.${currentBNLevel}`];
+    let key = ['BitNode'];
+    let value = [`${currentBN}.${currentBNLevel}`];
 
-      if (optionStates.player) {
-        const player = ns.getPlayer();
-        key = [...key, 'City', 'Karma', 'Kills', `Entropy`, `Mult`];
-        value = [
-          ...value,
-          `${player.city}`,
-          `${ns.formatNumber(ns.heart.break(), 3)}`,
-          `${ns.formatNumber(player.numPeopleKilled, 3)}`,
-          `${player.entropy}`,
-          `${ns.formatPercent(0.98 ** player.entropy, 2)}`
-        ];
-      }
-      if (optionStates.gang) {
-        ns.exec('gang_v3.js', 'home', { preventDuplicates: true });
-        optionStates.gang = false;
-      }
-      if (optionStates.crime) {
-        ns.exec('autoCrime.js', 'home', { preventDuplicates: true });
-        optionStates.crime = false;
-      }
-      if (optionStates.stock) {
-        ns.exec('stockControl.js', 'home', { preventDuplicates: true });
-        optionStates.stock = false;
-      }
-      if (optionStates.hacking) {
-        ns.exec('controlHack.js', 'home', { preventDuplicates: true });
-        optionStates.hacking = false;
-      }
-      if (optionStates.manageServers) {
-        ns.exec('extraServer_v2.js', 'home', { preventDuplicates: true });
-        optionStates.manageServers = false;
-      }
-      if (optionStates.graft) {
-        ns.exec('graft.js', 'home', { preventDuplicates: true });
-        optionStates.graft = false;
-      }
-      if (optionStates.graftMultiple) {
-        ns.exec('graftMultiple.js', 'home', { preventDuplicates: true });
-        optionStates.graftMultiple = false;
-      }
-      if (optionStates.blade) {
-        ns.exec('joinBlade.js', 'home', { preventDuplicates: true });
-        await ns.sleep(10);
-        ns.exec('blade_v4.js', 'home', { preventDuplicates: true });
-        optionStates.blade = false;
-      }
-      if (optionStates.stanek) {
-        ns.exec('stanek_v2.js', 'home', { preventDuplicates: true });
-        ns.exec('manageChargeStanek.js', 'home', { preventDuplicates: true });
-        ns.exec('spreadStanekCharger.js', 'home', { preventDuplicates: true });
-        optionStates.stanek = false;
-      }
-      if (optionStates.sleeve) {
-        ns.exec('sleeveChoosePresets.js', 'home', { preventDuplicates: true });
-        optionStates.sleeve = false;
-      }
-      if (optionStates.corp) {
-        ns.exec('corp_v2.js', 'home', { preventDuplicates: true });
-        optionStates.corp = false;
-      }
-      if (optionStates.runScript) {
-        const file = await ns.prompt(
-          'Choose script to run\nIgnore to exit',
-          { 'type': 'select', 'choices': ns.ls('home', '.js') }
-        );
-        if (file !== '') {
-          let thread = await ns.prompt('Thread count?', { 'type': 'text' });
-          thread = isNaN(parseInt(thread)) ? 1 : parseInt(thread);
-
-          const args = (await ns.prompt('Arguments?\nSeparate by spaces', { 'type': 'text' }))
-            .split(' ').filter(arg => arg !== '');
-
-          ns.exec(file, 'home', { preventDuplicates: true, threads: thread }, ...args);
-        }
-        optionStates.runScript = false;
-      }
-      if (optionStates.traveling) {
-        ns.exec('travel.js', 'home', { preventDuplicates: true });
-        optionStates.traveling = false;
-      }
-      if (optionStates.showRam) {
-        const max = ns.getServerMaxRam('home'),
-          used = ns.getServerUsedRam('home');
-        key = [...key, `Max`, `Used`, 'Free'];
-        value = [
-          ...value,
-          `${ns.formatRam(max, 2)}`,
-          `${ns.formatRam(used, 2)}`,
-          `${ns.formatRam(max - used, 2)}`,
-        ];
-      }
-      if (optionStates.buyRam) {
-        ns.exec('homeUpgrade.js', 'home', { preventDuplicates: true });
-        optionStates.buyRam = false;
-      }
-      if (optionStates.reportMoney) {
-        ns.exec('getMoneyReport.js', 'home', { preventDuplicates: true });
-        optionStates.reportMoney = false;
-      }
-      if (optionStates.hudRestart) ns.exec('restart.js', 'home', { preventDuplicates: true }, 'hudExtra_v2.js');
-      if (optionStates.hudExit) ns.exit();
-
-      hooks.hook0.innerText = key.join('\n');
-      hooks.hook1.innerText = value.join('\n');
-    } catch (e) {
-      ns.alert(e.cause + ' ');
+    if (optionStates.info) {
+      const player = ns.getPlayer(), max = ns.getServerMaxRam('home'), used = ns.getServerUsedRam('home');
+      key = [...key, 'City', 'Karma', 'Kills', `Entropy`, `Mult`, `Max`, `Used`, 'Free'];
+      value = [
+        ...value,
+        `${player.city}`,
+        `${ns.formatNumber(ns.heart.break(), 3)}`,
+        `${ns.formatNumber(player.numPeopleKilled, 3)}`,
+        `${player.entropy}`,
+        `${ns.formatPercent(0.98 ** player.entropy, 2)}`,
+        `${ns.formatRam(max, 2)}`,
+        `${ns.formatRam(used, 2)}`,
+        `${ns.formatRam(max - used, 2)}`,
+      ];
     }
+    if (optionStates.gang) {
+      ns.exec('gang_v3.js', 'home', { preventDuplicates: true });
+      optionStates.gang = false;
+    }
+    if (optionStates.crime) {
+      ns.exec('autoCrime.js', 'home', { preventDuplicates: true });
+      optionStates.crime = false;
+    }
+    if (optionStates.stock) {
+      ns.exec('stockControl.js', 'home', { preventDuplicates: true });
+      optionStates.stock = false;
+    }
+    if (optionStates.hacking) {
+      ns.exec('controlHack.js', 'home', { preventDuplicates: true });
+      optionStates.hacking = false;
+    }
+    if (optionStates.manageServers) {
+      ns.exec('extraServer_v2.js', 'home', { preventDuplicates: true });
+      optionStates.manageServers = false;
+    }
+    if (optionStates.grafting) {
+      const choice = await ns.prompt('Choose Grafting option:', { type: "select", choices: ['Graft', 'Multiple'] });
+      if (choice === 'Graft') ns.exec('graft.js', 'home', { preventDuplicates: true });
+      if (choice === 'Multiple') ns.exec('graftMultiple.js', 'home', { preventDuplicates: true });
+      optionStates.grafting = false;
+    }
+    if (optionStates.bnMult) {
+      ns.exec('BN_Mult.js', 'home', { preventDuplicates: true });
+      await ns.sleep(5);
+      try {
+        const terminalInput = doc.getElementById('terminal-input');
+        terminalInput.value = 'cat BitNodeMultipliers.txt';
+        const handler = Object.keys(terminalInput)[1];
+        terminalInput[handler].onChange({ target: terminalInput });
+        terminalInput[handler].onKeyDown({ key: 'Enter', preventDefault: () => null });
+      } catch { }
+      optionStates.bnMult = false;
+    }
+    if (optionStates.blade) {
+      ns.exec('joinBlade.js', 'home', { preventDuplicates: true });
+      await ns.sleep(5);
+      ns.exec('blade_v4.js', 'home', { preventDuplicates: true });
+      optionStates.blade = false;
+    }
+    if (optionStates.stanek) {
+      ns.exec('stanek_v2.js', 'home', { preventDuplicates: true });
+      ns.exec('manageChargeStanek.js', 'home', { preventDuplicates: true });
+      ns.exec('spreadStanekCharger.js', 'home', { preventDuplicates: true });
+      optionStates.stanek = false;
+    }
+    if (optionStates.sleeve) {
+      const choice = await ns.prompt('Choose Sleeve option:', { type: "select", choices: ['Presets', 'Contracts'] });
+      if (choice === 'Presets') ns.exec('sleeveChoosePresets.js', 'home', { preventDuplicates: true });
+      if (choice === 'Contracts') ns.exec('sleeveContracts.js', 'home', { preventDuplicates: true });
+      optionStates.sleeve = false;
+    }
+    if (optionStates.corp) {
+      ns.exec('corp_v2.js', 'home', { preventDuplicates: true });
+      optionStates.corp = false;
+    }
+    if (optionStates.runScript) {
+      const file = await ns.prompt(
+        'Choose script to run\nIgnore to exit',
+        { 'type': 'select', 'choices': ns.ls('home', '.js') }
+      );
+      if (file !== '') {
+        let thread = await ns.prompt('Thread count?', { 'type': 'text' });
+        thread = isNaN(parseInt(thread)) ? 1 : parseInt(thread);
+
+        const args = (await ns.prompt('Arguments?\nSeparate by spaces', { 'type': 'text' }))
+          .split(' ').filter(arg => arg !== '');
+
+        ns.exec(file, 'home', { preventDuplicates: true, threads: thread }, ...args);
+      }
+      optionStates.runScript = false;
+    }
+    if (optionStates.traveling) {
+      ns.exec('travel.js', 'home', { preventDuplicates: true });
+      optionStates.traveling = false;
+    }
+    if (optionStates.buyRam) {
+      ns.exec('homeUpgrade.js', 'home', { preventDuplicates: true });
+      optionStates.buyRam = false;
+    }
+    if (optionStates.reportMoney) {
+      ns.exec('getMoneyReport.js', 'home', { preventDuplicates: true });
+      optionStates.reportMoney = false;
+    }
+    if (optionStates.hud) {
+      const choice = await ns.prompt('Choose HUD option:', { type: "select", choices: ['Restart', 'Exit'] });
+      if (choice === 'Restart') ns.exec('restart.js', 'home', { preventDuplicates: true }, 'hudExtra_v2.js');
+      if (choice === 'Exit') ns.exit();
+    }
+
+    hooks.hook0.innerText = key.join('\n');
+    hooks.hook1.innerText = value.join('\n');
   }
 }
